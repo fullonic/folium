a883afc2ff2dcab92e35ae53c56c74b4554e0440
fullonic <somnium@riseup.net>
2019-04-02 19:46:06 +0200

    Remove from future imports

--- folium/plugins/locate.py
+++ folium/plugins/locate.py
@@ -1,7 +1,5 @@
 # -*- coding: utf-8 -*-
 
-from __future__ import (absolute_import, division, print_function)
-
 from branca.element import Figure, MacroElement
 from jinja2 import Template
 from folium import Popup
