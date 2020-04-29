d197de91aa861e0275b9d4825c07b00b618fced8
dbf <13336073+fullonic@users.noreply.github.com>
2020-04-29 15:22:48 +0200

    Update features.py
    Add live_update option to dynamicgeojson

--- folium/features.py
+++ folium/features.py
@@ -1256,7 +1256,7 @@ class GeoJsonPopup(GeoJsonDetail):
 
 
 class DynamicGeoJson(MacroElement):
-    """ GeoJson feature to consume live API data based on map bounds.
+    """GeoJson feature to consume live API data based on map bounds.
 
     Each time an event happen making the maps bound change, the four vertices of the map bounds will
     be saved in 4 variables: nW, nE, sE, sW. Using this 4 vertices, a API request can be made
@@ -1301,19 +1301,23 @@ class DynamicGeoJson(MacroElement):
     {% macro script(this, kwargs) %}
 
     // JS vars: Revision needed !!
-    {{ this._parent.parent_map.get_name() }}.on("{{ this.action }}", function() {
+    {% if this.live_update %}
+    {{ this._parent.parent_map.get_name() }}.on("{{ this.action }}",
+    {% else %}
+    $( "#reload" ).click({% endif %} function() {
     // This will get all map bounds and make all 2 points coordinates available for later use in API
     // call
 
     d = "{{ this.delimiter }}";
     latlng={{ this._parent.parent_map.get_name() }}.getBounds();
-    nW = latlng._southWest.{{ this.order[0] }} + d + latlng._northEast.{{ this.order[1] }}
-    nE = latlng._northEast.{{ this.order[0] }} + d + latlng._northEast.{{ this.order[1] }}
-    sE = latlng._northEast.{{ this.order[0] }} + d + latlng._southWest.{{ this.order[1] }}
-    sW = latlng._southWest.{{ this.order[0] }} + d + latlng._southWest.{{ this.order[1] }}
+    nE = latlng._northEast.{{ this.order[0] }} + "," + latlng._northEast.{{ this.order[1] }}
+    sW = latlng._southWest.{{ this.order[0] }} + "," + latlng._southWest.{{ this.order[1] }}
+    var bbox = sW + d + nE
+    console.log(bbox);
+
 
     // This will make a new rest api call.
-    var url = "{{ this.url_root }}" + {{ this.pattern }};
+    var url = "{{ this.url_root }}" + bbox;
     $.ajax({url: url, dataType: 'json', async: true,
         success: function(data) {
         {{ this._parent.get_name() }}.clearLayers();
@@ -1329,6 +1333,7 @@ class DynamicGeoJson(MacroElement):
     """
     )
 
+    # url/
     def __init__(
         self,
         action="moveend",
@@ -1336,7 +1341,9 @@ class DynamicGeoJson(MacroElement):
         url_pattern=None,
         order=("lng", "lat"),
         delimiter=",",
-    ):
+        live_update=True,
+        button_text="Search this area",
+    ):  # noqa
         super(DynamicGeoJson, self).__init__()
         self._name = "DynamicGeoJson"
         self.action = action
@@ -1344,7 +1351,37 @@ class DynamicGeoJson(MacroElement):
         self.pattern = url_pattern
         self.order = order
         self.delimiter = delimiter
+        self.live_update = live_update
+        self.button_text = button_text
 
+    def render(self, **kwargs):  # noqa
+        super(DynamicGeoJson, self).render(**kwargs)
+        figure = self.get_root()
+        assert isinstance(figure, Figure), (
+            "You cannot render this Element " "if it is not in a Figure."
+        )
+
+        button_style = """
+            <style>
+                #reload {
+                    position: absolute;
+                    z-index: 999;
+                    padding: 6px;
+                    right: 48%;
+                    font-size: 12px;
+                    text-decoration: none;
+                    top: 3%;
+                }
+            </style>
+        """
+        button_reload = """
+        <button type="button" id="reload" class="btn btn-default btn-sm">{0}</button""".format(
+            self.button_text
+        )  # noqa
+
+        if not self.live_update:
+            figure.header.add_child(Element(button_style), name="reload")
+            figure.html.add_child(Element(button_reload), name="button_reload")
 
 class Choropleth(FeatureGroup):
     """Apply a GeoJSON overlay to the map.
